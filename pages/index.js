36584bacc15b3dd944b64f853e8e3e6230e06aae
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-28 18:30:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,6 @@ import { projects } from '../utils/projectsData';
 const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
-  <Image
-      src={logoimg}
-      alt="Picture of the author"
-      // width={500} automatically provided
-      // height={500} automatically provided
-      // blurDataURL="data:..." automatically provided
-      // placeholder="blur" // Optional blur-up while loading
-    />
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
