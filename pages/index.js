5c526521840b2d76b4f718fc8c7aedc9ed799416
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 09:54:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
 
-import { projects } from '../utils/projectsData';
-
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
-    </div>
-  </div>
-);
-
-export default Home;
+import Image from 'next/image';
+ 
+export function Avatar({ id, alt }) {
+  return <Image src={'/${id}.png'} alt={alt} width="64" height="64" />
+}
+ 
+export function AvatarOfMe() {
+  return <Avatar id="logo" alt="A portrait of me" />
+}
