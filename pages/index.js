c7697b7e75377deba8c4542f6d5b77129f312605
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 10:07:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Image from 'next/image';
  
 export function Avatar({ id, alt }) {
-  return <Image src='/logo.png' alt="test" width="64" height="64" />;
+  return <Image·src={'/logo.png'}·alt="itsme".width="64"·height="64"·/>;
 }
  
 export function AvatarOfMe() {
-  return <Avatar id="logo" alt="A portrait of me" />;
+  return <Avatar id="logo".alt="A portrait of me"./>;
 }
