20cee67bc9f586741a5b9748d2a1d39f8beb5096
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 14:42:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Image from 'next/image'
+import Image from "next/image";
  
-export function Avatar({ id, alt }) {
-  return <Image src=" 
-https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png"
-                    alt="GFG logo served with static path of public directory"
-                    height="100"
-                    width="400"
-                /> 
- 
-export function AvatarOfMe() {
-  return <Avatar id="me" alt="A portrait of me" />
-}
+const index = () => {
+    return (
+        <>
+            <h1 style={{ color: 'green' }}>GeeksForGeeks</h1>
+            <Image src="/logo.png" alt="Loading"
+                width={500}
+                height={550}
+                quality="100"
+            />
+        </>
+    );
+};
+export default index;
