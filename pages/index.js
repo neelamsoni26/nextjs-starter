4cd730c3bb547b42dd64ad54c00490d876d538bc
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 12:55:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,7 @@ import profilePic from '../public/logo.png';
  
 export default function Page() {
   return (
-    <Image
-      src={profilePic}
-      alt="Picture of the author"
-      // width={500} automatically provided
-      // height={500} automatically provided
-      // blurDataURL="data:..." automatically provided
-      // placeholder="blur" // Optional blur-up while loading
-    />;
+    <Image src={profilePic} alt="Picture of the author" />;
   )
 }
 
