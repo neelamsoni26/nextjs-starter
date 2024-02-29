d57f266f6075fd2db24141247f79f25224e039b0
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 15:06:04 +0530

    Update Icons.js

--- components/Icons.js
+++ components/Icons.js
@@ -247,7 +247,7 @@ export function GithubIcon() {
 
 export function msICON() {
   return (
-    <Image src={"/logo.png"} alt="image" width="64" height="64"·/>;
+    <Image src={"/logo.png"} alt="image" width="64" height="64" />;
   );
 }
 
