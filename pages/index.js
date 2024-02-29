6e81e82da586a84ff685dae2877ba1ad5821e4a8
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 12:53:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import profilePic from '../public/logo.png'
+import profilePic from '../public/logo.png';
  
 export default function Page() {
   return (
@@ -10,7 +10,7 @@ export default function Page() {
       // height={500} automatically provided
       // blurDataURL="data:..." automatically provided
       // placeholder="blur" // Optional blur-up while loading
-    />
+    />;
   )
 }
 
