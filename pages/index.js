c3e589a9f692f092ac0b0990a439080fd7844806
neelamsoni26 <108008472+neelamsoni26@users.noreply.github.com>
2024-02-29 14:14:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,6 @@ import Image from 'next/image';
 import profilePic from '../public/logo.png';
  
 export default function Page() {
-  return (
- <Image
-    src={"../public/logo.png"}
-    alt="image"
-    width={80}
-    height={80}
-  />
-  )
+  return (   <Image src={'/logo.png'} alt={alt} width="64" height="64" /> )
 }
 
